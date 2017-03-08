a14762b6b8854a27725422de25dd0030cb9e4813
xinyu <zhangxinyu019@gmail.com>
2017-03-08 04:55:35 -0800

    fuzzification

--- stocktrading/src/Fuzzy.java
+++ stocktrading/src/Fuzzy.java
@@ -6,122 +6,128 @@ public class Fuzzy {
     public static void main(String[] args) {
         float price;
         float mad;
-        String trade;
-        int tradeAmount;
         float money;
+        float stock = 0;
         float totalMoney = 10000;
         float r = randomWithRange(-1, 1);
-        for (int i = 0; i < 150; i++) {
-            if(totalMoney > 0) {
-                price = getPrice(i,r);
-                mad = getMad(i, r);
-                trade = ruleBase(fuzzyPrice(price), fuzzyMad(mad));
-                tradeAmount = stockTrade(trade);
-                money = tradeAmount * price;
-                totalMoney += money;
-                System.out.println("The current price is: " + price + ", price is " + fuzzyPrice(price));
-                System.out.println("The current mad is: " + mad + ", MAD is " + fuzzyMad(mad));
-                System.out.println("The trade amount is: " + tradeAmount + " and money is: " + money);
-                System.out.println("The total money is: " + totalMoney);
+        for(int i = 0; i < 150; i++) {
+            price = getPrice(i,r);
+            mad = getMAD(i,r);
+            float priceArray[] = fuzzyPrice(price);
+            float madArray[] = fuzzyMAD(mad);
+            float ruleBase[][] = ruleBase(priceArray, madArray);
+            float cog = cogOutput(ruleBase);
+            //cog negtive sell, cog positive buy
+//            if(cog < 0 && stock < Math.abs(stock)) {
+//                cog = -stock;
+//            }
+            money = calculate(cog,price);
+            totalMoney = totalMoney - money;
+//            stock = stock + cog;
+            if(totalMoney < 0){
+                System.out.println("Your money is not enough! Please wait to next trade.");
+                totalMoney = totalMoney + money;
+//                stock = stock - cog;
+                i++;
             }
-            else
-                System.out.println("you have no money");
+            System.out.println("For the " + i +"th day, the money you have is " + totalMoney + ", the stocks you have is "+ stock);
         }
-
     }
     private static float getPrice(int i, float r) {
         float p = (float)(10 + 2.5 * Math.sin(Math.PI*2*i/19) + 0.8 * Math.cos(Math.PI*2*i/5) + 7 * r * (i%2));
         return p;
     }
-    private static float getMad(int i, float r) {
+    private static float getMAD(int i, float r) {
         float m = (float)(0.5 * Math.cos(0.3*i) - Math.sin(0.3*i) + 0.4 * r * (i%3));
         return m;
     }
-    private static String fuzzyPrice(float price) {
-        float priceInterval = (float)(20.3 + 0.3)/ 5;
-        float p = price + (float)0.3;
-        if (p >= 0 && price <= 1 * priceInterval)
-            return "VL";
-        else if (p > 1 * priceInterval && p <= 2 * priceInterval)
-            return "LO";
-        else if (p > 2 * priceInterval && p <= 3 * priceInterval)
-            return "MD";
-        else if (p > 3 * priceInterval  && p <= 4 * priceInterval)
-            return "HI";
-        else if (p > 4 * priceInterval && p <= 20.6)
-            return "VH";
-        else
-            return "Unexpected";
-    }
-    private static String fuzzyMad(float mad) {
-        float madInterval = (float)4.6 / 3;
-        float m = mad + (float)2.3;
-        if(m >= 0 && m <= 1 * madInterval)
-            return "P";
-        else if(m > madInterval && m <= 2 * madInterval)
-            return "Z";
-        else if(m > 2 * madInterval && m <= 4.6)
-            return "N";
-        else
-            return "Unexpected";
-    }
-    private static String ruleBase(String price, String mad) {
-        if(price.equals("VL") && mad.equals("P"))
-            return "BM";
-        else if(price.equals("VL") && mad.equals("Z"))
-            return "BM";
-        else if(price.equals("VL") && mad.equals("N"))
-            return "DT";
-        else if(price.equals("LO") && mad.equals("P"))
-            return "BM";
-        else if(price.equals("LO") && mad.equals("Z"))
-            return "BF";
-        else if(price.equals("LO") && mad.equals("N"))
-            return "SF";
-        else if(price.equals("MD") && mad.equals("P"))
-            return "BF";
-        else if(price.equals("MD") && mad.equals("Z"))
-            return "DT";
-        else if(price.equals("MD") && mad.equals("N"))
-            return "SF";
-        else if(price.equals("HI") && mad.equals("P"))
-            return "BF";
-        else if(price.equals("HI") && mad.equals("Z"))
-            return "SF";
-        else if(price.equals("HI") && mad.equals("N"))
-            return "SM";
-        else if(price.equals("VH") && mad.equals("P"))
-            return "DT";
-        else if(price.equals("VH") && mad.equals("Z"))
-            return "SM";
-        else if(price.equals("VH") && mad.equals("N"))
-            return "SM";
-        else
-            return "Unexpected";
-    }
-    private static int stockTrade(String trade) {
-        int x = 0;
-        switch(trade) {
-            case "BM":
-                x = 800;
-                break;
-            case "BF":
-                x = 400;
-                break;
-            case "MD":
-                x = 0;
-                break;
-            case "SM":
-                x = -800;
-                break;
-            case "SF":
-                x = -400;
-                break;
+
+    private static float[] fuzzyPrice(float price) {
+        // priceArray {'VL','LO','MD','HI','VH'}
+        float priceArray[];
+        priceArray = new float[5];
+        if (price <= 5)
+            priceArray[0] = 1;
+        else if (price > 5 && price <= 7) {
+            priceArray[0] = (float) (-0.5 * price + 3.5);
+            priceArray[1] = (float) (0.5 * price - 2.5);
+        } else if (price > 7 && price <= 9) {
+            priceArray[1] = (float) (-0.5 * price + 4.5);
+            priceArray[2] = (float) (0.5 * price - 3.5);
+        } else if (price > 9 && price <= 11) {
+            priceArray[2] = (float) (-0.5 * price + 5.5);
+            priceArray[3] = (float) (0.5 * price - 4.5);
+        } else if (price > 11 && price <= 13) {
+            priceArray[3] = (float) (-0.5 * price + 6.5);
+            priceArray[4] = (float) (0.5 * price - 5.5);
+        } else {
+            priceArray[4] = 1;
         }
-        return x;
+        return priceArray;
+    }
+
+    private static float[] fuzzyMAD(float mad) {
+        //madArray {'N','Z','P'}
+        float madArray[];
+        madArray = new float[3];
+        if(mad <= -1)
+            madArray[0] = 1;
+        else if (mad > -1 && mad <= -0.5) {
+            madArray[0] = - mad;
+    }
+        else if (mad > -0.5 && mad <= 0) {
+            madArray[0] = - mad;
+            madArray[1] = 2 * mad + 1;
+        }
+        else if (mad > 0 && mad <= 0.5) {
+            madArray[1] = -2 * mad + 1;
+            madArray[2] = mad;
+        }
+        else if (mad > 0.5 && mad <= 1) {
+            madArray[2] = mad;
+        } else {
+            madArray[2] = 1;
+        }
+        return madArray;
+    }
+    private static float[][] ruleBase(float p[], float m[]) {
+        //rule base table { row: MAD fuzzy set, col: price fuzzy set}
+        float ruleTable[][];
+        ruleTable = new float[3][5];
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 3; j++) {
+                ruleTable[j][i] = Math.min(p[i],m[j]);
+            }
+        }
+        return ruleTable;
+    }
+    private static float cogOutput(float t[][]) {
+        //trade decision {SM:-800 ~ -200},{SF:-400 ~ 0},{DT:-200 ~ 200}
+        //{BF:0 ~ 400}, {BM:200 ~ 800}
+        float sm = Max(t[0][3],t[0][4],t[1][4]);
+        float sf = Max(t[0][1],t[0][2],t[1][3]);
+        float dt = Max(t[0][0],t[1][2],t[2][4]);
+        float bf = Max(t[1][1],t[2][2],t[2][3]);
+        float bm = Max(t[1][0],t[2][0],t[2][1]);
+        float cogX = (-800-700-600-500-400-300-200)*sm + (-400-300-200-100)*sf + (-200-100+100+200)*dt + (100+200+300+400)*bf + (200+300+400+500+600+700+800)*bm;
+        float cogY = sm*7 + sf*4 + dt*4 + bf*4 + bm*7;
+        float cog = (float)Math.ceil(cogX/cogY);
+        return cog;
     }
     private static float randomWithRange(int min, int max) {
         float range = max - min;
         return (float)(Math.random() * range + min);
+    }
+    private static float Max(float x, float y, float z) {
+        float max = Math.max(Math.max(x,y),z);
+        return max;
+    }
+    private static float calculate(float cog, float p) {
+        float m;
+        m = cog * p;
+        if(m < -800) m = -800;//sell
+        else if(m > 800) m = 800;//buy
+        else m = m;
+        return m;
     }
 }
